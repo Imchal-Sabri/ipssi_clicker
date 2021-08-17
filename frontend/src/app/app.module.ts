2ca416c234aacbf9a5952f82a6a63986c17cbb63
d4nm0 <dmonceau@laposte.net>
2021-08-17 11:30:07 +0200

    deplacement des fichiers dans un dossier frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { ScoreComponent } from './score/score.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterComponent,
+    ScoreComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  point: number = 0;
+
+  constructor() {}
+
+  effect(e: any) {
+    let body = document.getElementsByClassName('characters-img')[0];
+    body.classList.add('move');
+  }
+
+  ngOnInit(): void {}
+
+  Point(e: any) {
+    // console.log(e)
+    this.point = e;
+  }
+}
